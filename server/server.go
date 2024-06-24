daf930c1ca6897bbc95008cbc547789be39ccf3a
Richard Sullivan <richardsullivan@Richards-MBP.lan>
2024-06-23 19:21:28 -0500

    feat: redirect root to contacts page
    Added a request handler for /contacts, and updated the handler for root to
    redirect to the contacts page.

--- server/server.go
+++ server/server.go
@@ -13,14 +13,24 @@ func printServer() {
 
 func StartServer() {
 	// create handler for the server
-	http.HandleFunc("/", helloHandler)
+	mux := http.NewServeMux()
+
+	// adding handlers here
+	mux.Handle("/", http.HandlerFunc(rootHandler))
+	mux.Handle("/contacts", http.HandlerFunc(contactsHandler))
 
 	// log that the server stopped when this exits
 	printServer()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.URL)
-	io.WriteString(w, "Hello, World\n")
+// handle the root request by redirecting clients to the contacts page
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("rootHandler:", req.URL)
+	http.Redirect(w, req, "contacts", http.StatusMovedPermanently)
+}
+
+func contactsHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("contactsHandler", req.URL)
+	io.WriteString(w, "Contacts Page\n")
 }
